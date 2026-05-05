244103c6313e45acc771d8017c07da50e8cdc0d8
Adaeze Anigbata <adatori36@gmail.com>
2026-05-06 00:16:54 +0100

    Fix terraform formatting in ekscluster.tf

--- terraform/ekscluster.tf
+++ terraform/ekscluster.tf
@@ -9,7 +9,7 @@ module "eks" {
   subnet_ids = module.vpc.private_subnets
 
   cluster_endpoint_public_access = true
-  
+
   enable_cluster_creator_admin_permissions = true
   eks_managed_node_group_defaults = {
     ami_type = "AL2023_x86_64_STANDARD"
